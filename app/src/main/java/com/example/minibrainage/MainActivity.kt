4db909c6923ee57800ee15b547c8a99570541975
Abhiek187 <achaudhuri2011@yahoo.com>
2020-09-17 17:04:10 -0400

    Make canvas square and add equation

--- app/src/main/java/com/example/minibrainage/MainActivity.kt
+++ app/src/main/java/com/example/minibrainage/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.minibrainage
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintSet
 import com.example.minibrainage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var answer = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,19 +20,74 @@ class MainActivity : AppCompatActivity() {
         val layoutPage = binding.layoutPage
         val buttonClassify = binding.buttonClassify
         val buttonReset = binding.buttonReset
+        val textViewMath = binding.textViewMath
+
+        // Generate a random math equation to solve
+        textViewMath.text = generateRandomEquation()
 
         // Add CanvasView to ConstraintLayout
         val canvasView = CanvasView(this)
+        canvasView.id = View.generateViewId() // generate id to apply constraints
+        canvasView.setBackgroundColor(Color.WHITE)
         layoutPage.addView(canvasView)
+
+        // Make CanvasView square, height = phone width - margins, middle of screen
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(layoutPage)
+        // Set width and height to match constraint
+        constraintSet.constrainWidth(canvasView.id, 0)
+        constraintSet.constrainHeight(canvasView.id, 0)
+        // Put view in the middle of the screen
+        constraintSet.connect(canvasView.id, ConstraintSet.START, layoutPage.id, ConstraintSet.START, 8)
+        constraintSet.connect(canvasView.id, ConstraintSet.END, layoutPage.id, ConstraintSet.END, 8)
+        constraintSet.connect(canvasView.id, ConstraintSet.TOP, buttonClassify.id, ConstraintSet.BOTTOM, 8)
+        constraintSet.connect(canvasView.id, ConstraintSet.BOTTOM, textViewMath.id, ConstraintSet.TOP, 8)
+        // Make view a square
+        constraintSet.setDimensionRatio(canvasView.id, "1:1")
+        constraintSet.applyTo(layoutPage)
 
         buttonClassify.setOnClickListener {
             // Classify the number drawn
-            Toast.makeText(this, "You drew something.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "The correct answer is $answer.", Toast.LENGTH_SHORT).show()
         }
 
         buttonReset.setOnClickListener {
             // Clear the canvas
             canvasView.clear()
+            // Generate a new equation
+            textViewMath.text = generateRandomEquation()
         }
+    }
+
+    private fun generateRandomEquation(): String {
+        // Make the first number, second number, and operator random and save the answer
+        val a = (0..99).random() // 0-99
+        val b = (0..99).random()
+        val op: Char
+
+        when ((0..4).random()) {
+            0 -> {
+                op = '+'
+                answer = a + b
+            }
+            1 -> {
+                op = '-'
+                answer = a - b
+            }
+            2 -> {
+                op = '*'
+                answer = a * b
+            }
+            3 -> {
+                op = '/'
+                answer = a / b
+            }
+            else -> {
+                op = '%'
+                answer = a % b
+            }
+        }
+
+        return "$a $op $b = ?"
     }
 }
