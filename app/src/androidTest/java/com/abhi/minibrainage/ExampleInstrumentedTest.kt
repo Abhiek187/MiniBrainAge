61bf515d7edd4640ca7ad23771aa4228d2c2c0ad
Abhiek187 <achaudhuri2011@yahoo.com>
2021-11-20 23:00:01 -0500

    Change the games ids and remove all traces of com.example

--- app/src/androidTest/java/com/abhi/minibrainage/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/abhi/minibrainage/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.minibrainage", appContext.packageName)
+        assertEquals("com.abhi.minibrainage", appContext.packageName)
     }
 }
